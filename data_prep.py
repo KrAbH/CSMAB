9ee90881f8de7b9cb6d32c16e7d69a49e3ce9321
KrAbH <kumar.abhishek@research.iiit.ac.in>
2020-12-24 12:38:25 +0530

    Added data preparation file to generate mean of reward and availability

---
+++ data_prep.py
@@ -0,0 +1,39 @@
+import numpy as np
+import random 
+import math
+from scipy.stats import bernoulli
+import sys
+import json
+
+#k: Number of arms
+k = 5
+
+########################################################################
+#Reward true mean
+#range within which true mu belongs
+rs = 0.2
+re = 0.9
+mu_arr = np.random.uniform([rs]*k, [re]*k)
+mu_file = 'mu.txt'
+with open(mu_file, 'w') as outfile:
+	for i in mu_arr:
+		outfile.write('%f\n' % i)
+
+########################################################################
+
+########################################################################
+#Availability mean
+#range within which mean belongs
+rs = 0.3
+re = 0.7
+mu_arr = np.random.uniform([rs]*k, [re]*k)
+mu_file = 'availability.txt'
+with open(mu_file, 'w') as outfile:
+	for i in mu_arr:
+		outfile.write('%f\n' % i)
+
+########################################################################
+
+
+
+
