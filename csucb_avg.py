07dec5b49d68fdf482f9f4a8c9cd9c000d67235c
KrAbH <kumar.abhishek@research.iiit.ac.in>
2020-12-25 16:01:42 +0530

    Fixed the bug in the code; tested for sample case; output sample in out1.txt

--- csucb_avg.py
+++ csucb_avg.py
@@ -7,6 +7,7 @@ import json
 import matplotlib.pyplot as plt
 import time
 
+start_time = time.time()
 N = int(sys.argv[1]) #N is number of epochs
 T = int(sys.argv[2]) #T is number of rounds in one epoch
 mu_file = 'mu.txt'
@@ -32,6 +33,7 @@ for i in range(T):
 
 alpha = 0.9
 for j in range(N):
+	
 	num_pulls = np.zeros(n, dtype = float)
 	tot_rew    = np.zeros(n, dtype = float)
 	#expl_term = np.zeros(n, dtype = float)
@@ -43,7 +45,11 @@ for j in range(N):
 
 	for i in range(T):
 		#Generate the availability of arms considering it as bernoulli random variable
-		A_t  = bernoulli(avl)
+		#print ("ucb_val", ucb_val)
+
+		A_t  = np.random.binomial(1, avl)
+		#print("A_t", A_t)
+		#print (type(A_t), A_t)
 		fl = 0
 		for k in range(n):
 			if A_t[k]==1:
@@ -54,7 +60,7 @@ for j in range(N):
 			continue
 		t_ucb =  np.multiply(A_t, ucb_val)
 		t_mu  =  np.multiply(A_t, mu)
-
+		#print("t_ucb", t_ucb)
 		#Find the index of arm with max ucb value
 		mx_ind = -1
 		mx = -100000
@@ -63,16 +69,21 @@ for j in range(N):
 				mx = t_ucb[k]
 				mx_ind = k
 		I_t = mx_ind
-		rew = bernoulli(mu[I_t])
+		#print (I_t)
+		rew = np.random.binomial(1, mu[I_t])
 		#Update 
 		tot_rew[I_t] = rew
 		num_pulls[I_t] += 1
 		expl = (3* math.log10(i+1))/ (2*num_pulls[I_t]) 
 		temp_ucb = tot_rew[I_t]/num_pulls[I_t] + math.sqrt(expl)
-		ucb_val[k] = temp_ucb
+		ucb_val[I_t] = temp_ucb
 
 		a_star = np.amax(t_mu)
 		regret[i] += a_star - mu[I_t]
+		#print (I_t, a_star, mu[I_t])
+	print(j)
+	print("--- %s seconds ---" % (time.time() - start_time))
+	start_time = time.time()
 
 out = "out1.txt"
 with open(out, 'w') as outfile:
